c44c78e1796f333a0f8718abaf3a93d7fc3ef808
Adam Rasfeld <arasfeld@gmail.com>
2024-04-08 11:12:48 -0400

    update startup screen

--- src/screens/Startup.tsx
+++ src/screens/Startup.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
-import { Heading, SafeScreen, Text } from '@/components';
+import { StyleSheet, View } from 'react-native';
+import { Button, Heading, SafeScreen, Text, WelcomingSvg } from '@/components';
+import { useTheme } from '@/theme';
 
 function Startup(): React.JSX.Element {
+  const { colors, layout } = useTheme();
+
   return (
     <SafeScreen>
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <Heading>Heading</Heading>
-        <Text>Content</Text>
-      </ScrollView>
+      <View style={[layout.flex1, layout.gap12, layout.itemsCenter, layout.justifyCenter, layout.padding8]}>
+        <Heading>First of all, congrats!</Heading>
+        <Text>You've just taken a big step on the path to sobriety.</Text>
+        <WelcomingSvg color={colors.primary.main} height="70%" width="70%" />
+        <Button style={styles.button} variant="solid">Get Started</Button>
+      </View>
     </SafeScreen>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    width: '100%',
+  },
+});
 
 export default Startup;
